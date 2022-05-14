97b12d418cb2761205225d3f76805c86d8b427a4
dominichayesferen <ferenosdev@outlook.com>
2022-05-14 22:49:00 +0100

    Now overwrites the styling of Customise's buttons

--- mod-files/user.js
+++ mod-files/user.js
@@ -13,3 +13,4 @@ user_pref("widget.gtk.alt-theme.scrollbar_active", false);
 user_pref("widget.gtk.alt-theme.selection", false);
 user_pref("widget.gtk.overlay-scrollbars.enabled", false);
 user_pref("widget.gtk.theme-scrollbar-colors.enabled", false);
+user_pref("layout.css.color-mix.enabled", true);
